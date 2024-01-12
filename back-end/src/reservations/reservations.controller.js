4782c7e8a94258ceb38fd31d0b5886bf8010b11f
Collin <collindewalt@gmail.com>
2024-01-11 23:38:47 -0500

    completed US1backend
    passed all backend tests.  Can't get the error message to work correctly if their are no reservations for the queried date

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,27 +1,96 @@
+const reservationsService = require("./reservations.service.js");
+
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const hasProperties = require("../errors/hasProperties");
+const hasRequiredProperties = hasProperties(
+  "first_name", 
+  "last_name", 
+  "mobile_number", 
+  "reservation_date", 
+  "reservation_time", 
+  "people"
+);
+
+const VALID_PROPERTIES = [
+  "first_name", 
+  "last_name", 
+  "mobile_number", 
+  "reservation_date", 
+  "reservation_time", 
+  "people",
+];
+
+async function hasOnlyValidProperties(req, res, next) {
+  const { data = {} } = req.body;
+
+  const invalidFields = Object.keys(data).filter((field) => {
+    if (!VALID_PROPERTIES.includes(field)) {
+      return true;
+    }
+
+    if (field === 'reservation_date') {
+      return isNaN(new Date(data[field]).getTime());
+    }
+
+    if (field === 'reservation_time') {
+      const timeRegex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+      return !timeRegex.test(data[field]);
+    }
+
+    return false;
+  });
+
+  if (invalidFields.length) {
+    return next({
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
+    });
+  }
+
+  next();
+}
+
+
+
+function peopleIsValidNumber(req, res, next) {
+  const { data: { people } = {} } = req.body;
+
+  if (isNaN(people) || people <= 0 || !Number.isInteger(people)) {
+    return next({
+      status: 400,
+      message: "people must be a valid positive integer",
+    });
+  }
+
+  next();
+}
+
+
+
 async function create(req, res, next) {
-  const {data: {first_name, last_name, mobile_number, reservation_date, reservation_time, people} = {} } = req.body;
-  const newReservation = {
-    id: id+1,
-    first_name: first_name,
-    last_name: last_name,
-    mobile_number: mobile_number,
-    reservation_date: reservation_date,
-    reservation_time: reservation_time,
-    people: people,
-  };
-  res.status(201).json({ data: newReservation});
+  reservationsService
+    .create(req.body.data)
+    .then((data) => res.status(201).json({ data }))
+    .catch(next);
+
 }
 
 /**
  * List handler for reservation resources
  */
-async function list(req, res) {
-  res.json({
-    data: [],
-  });
+async function list(req, res, next) {
+  const { date } = req.query;
+  const reservationDate = reservationsService.list(date);
+  reservationDate
+    .then((data) => res.json({ data }))
+    .catch(next);
+  if (!reservationDate) {
+    return res.json({message: `There are no reservations scheduled for ${date}.`});
+  }
 }
+
 
 module.exports = {
   list,
-  create,
+  create: [hasOnlyValidProperties, hasRequiredProperties, peopleIsValidNumber, create],
 };
---
+++ back-end/src/reservations/reservations.service.js
@@ -0,0 +1,25 @@
+const knex = require("../db/connection");
+
+async function create(reservation) {
+  return knex("reservations")
+    .insert(reservation)
+    .returning("*")
+    .then((createdRecords) => createdRecords[0]);
+}
+
+async function list(date) {
+  let query = knex("reservations").select("*");
+
+  if (date) {
+    query = query.whereRaw('CAST(reservation_date AS DATE) = ?', [date]);
+  }
+
+  return query.orderBy(['reservation_date', 'reservation_time'], 'asc');
+}
+
+
+
+module.exports = {
+  list,
+  create,
+};
